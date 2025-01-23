6b289b9f5b5247a17288cb76acc86500c08ad041
Damyan <d.peychev12@gmail.com>
2025-01-23 11:06:59 +0100

    i forgor

---
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    [SerializeField] private GameObject keyCapPrefab;
+    [SerializeField] private Vector3 offset = new Vector3 (0, 10, 0);
+    public float radius = 3f;
+    [HideInInspector] public bool interacted;
+
+    private Transform player;
+    private GameObject keyCap;
+
+    private void Start ()
+    {
+        
+    }
+
+    private void OnDrawGizmosSelected ()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere (transform.position, radius);
+    }
+
+    private void Update ()
+    {
+        if (player == null) player = GameObject.FindGameObjectWithTag ("Player").transform;
+
+        Interact ();
+        ShowKeyCap ();
+    }
+
+    protected virtual void Interact ()
+    {
+        
+    }
+
+    private void ShowKeyCap ()
+    {
+        if (keyCap == null && Vector3.Distance (player.position, transform.position) < radius)
+        {
+            keyCap = Instantiate (keyCapPrefab, transform.position + offset, Quaternion.identity, transform.GetChild(0).transform);
+        } else if (keyCap != null && Vector3.Distance (player.position, transform.position) > radius)
+        {
+            Destroy (keyCap);
+        }
+
+        if (keyCap != null) keyCap.transform.LookAt (Camera.main.transform.position);
+    }
+}
---
+++ Assets/Scripts/Interactables/InteractableObject.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+
+public class InteractableObject : Interactable
+{
+}
